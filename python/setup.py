c8077d871a2d0d9be1fb57e60a4937ef43b2838e
Zhymabek Roman <61125068+ZhymabekRoman@users.noreply.github.com>
2023-12-22 19:01:01 +0600

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     rust_extensions=[RustExtension('sqlite_zstd.libsqlite_zstd',
         path='../Cargo.toml',
@@ -8,4 +11,6 @@ setup(
         features=['build_extension'],
         py_limited_api=True,
     )],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
